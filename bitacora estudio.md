2e44ea0a08827131de3d5260a41912a51496ed6b
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-09-04 14:57:16 -0300

    formulario para tareas

---
+++ bitacora estudio.md
@@ -0,0 +1,5 @@
+# BITACORA
+
+link youtube: https://www.youtube.com/watch?v=_SPoSMmN3ZU
+
+siguiente sesion: formulario (3:26:26)
--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,7 +1,30 @@
 const NewPage = () => {
   return (
-    <div>
-      <h1>Formulario de tareas</h1>
+    <div className="h-screen flex justify-center items-center">
+      <form className="bg-slate-800 p-10 w-1/4">
+        <h1 className="py-2 text-2xl text-bold">Formulario de tareas</h1>
+        <label htmlFor="title" className="font-bold text-sm">
+          Titulo de la tarea
+        </label>
+        <input
+          type="text"
+          className="border border-gray-400 p-2 mb-4 w-full text-black"
+          placeholder="titulo"
+          id="title"
+        />
+        <label htmlFor="description" className="font-bold text-sm">
+          Descripcion de la tarea
+        </label>
+        <textarea
+          className="border border-gray-400 p-2 mb-4 w-full text-black"
+          rows="3"
+          placeholder="Describe tu tarea"
+          id="description"
+        ></textarea>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Crear
+        </button>
+      </form>
     </div>
   );
 };
