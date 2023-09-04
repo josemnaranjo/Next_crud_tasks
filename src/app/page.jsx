6823253ddbd346a7572533f89a8a46e37ab08886
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-09-04 15:47:59 -0300

    pagina principal

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,37 @@
+//Con Next podemos tener el front y el back en el mismo proyecto. Dado esto, hacer una llamado a nuestra BD desde un componente que este renderizado en el servidor.
+
+// async function loadTasks () {
+//     const res = await fetch("http://localhost:3000/api/tasks")
+//     const data = await res.json();
+//     return data
+// }
+
+"use client";
+import { useState, useEffect } from "react";
+
 const HomePage = () => {
+  //const tasks =  await loadTasks(). Recuerda volver este componente async.
+  const [tasks, setTasks] = useState([]);
+  const getTasks = async () => {
+    const res = await fetch("/api/tasks", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const tasks = res.json();
+    return tasks;
+  };
+
+  useEffect(() => {
+    getTasks().then((val) => setTasks(val));
+  }, []);
   return (
     <div>
       <h1>Tareas</h1>
+      {tasks?.map((task) => (
+        <div key={task.id}>{task.title} </div>
+      ))}
     </div>
   );
 };
