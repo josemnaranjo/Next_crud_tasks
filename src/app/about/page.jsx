bd8c068db56209473a596e00dbf4de06000b15ab
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-09-06 17:11:03 -0300

    diseño

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,6 +2,12 @@ const AboutPage = () => {
   return (
     <div>
       <h1>About</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus
+        tempore quasi nulla placeat porro, debitis nostrum ducimus, labore
+        provident blanditiis ea est veniam minus non pariatur tenetur natus?
+        Voluptates, optio.
+      </p>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-around p-3 bg-slate-600">
-      <h3 className="text-xl font-semibold">NextCRUD</h3>
+      <Link href="/">
+        <h3 className="text-xl font-semibold">NextCRUD</h3>
+      </Link>
 
       <ul className="flex gap-3">
         <li className="cursor-pointer hover:underline hover:underline-offset-2">
